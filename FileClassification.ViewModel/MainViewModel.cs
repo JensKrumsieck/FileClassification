3f80cd7d1f2cfc92406d5e4d686711b1bbeff359
Jens Krumsieck <jenso666@live.de>
2022-08-18 13:32:14 +0200

    more fields

--- FileClassification.ViewModel/MainViewModel.cs
+++ FileClassification.ViewModel/MainViewModel.cs
@@ -11,15 +11,27 @@ namespace FileClassification.ViewModel;
 
 public class MainViewModel : BaseViewModel
 {
-    [JsonIgnore] public override string Title { get; set; } = "";
+    private string _axialLigand = "";
 
-    [JsonIgnore]
-    public ObservableCollection<string> Classes { get; } = new();
+    private string _class = "";
 
-    private Stack<string> _files;
-    private string _workingDir = "";
+    private int _coordNo;
+
+    private string _coSolv = "";
 
     private Molecule _currentMolecule;
+
+    private Stack<string> _files;
+    private string _ligand = "";
+
+    private string _metal = "";
+
+    private int _substNo;
+    private string _workingDir = "";
+
+    [JsonIgnore]
+    public ObservableCollection<string> Classes { get; } =
+        new() {"Corrole", "Porphyrin", "Norcorrole", "Porphycene", "Corrphycene"};
 
     [JsonIgnore]
     public Molecule CurrentMolecule
@@ -28,51 +40,51 @@ public class MainViewModel : BaseViewModel
         set => Set(ref _currentMolecule, value);
     }
 
-    private string _metal = "";
-    
     public string Metal
     {
         get => _metal;
         set => Set(ref _metal, value);
     }
 
-    private string _axialLigand = "";
+    public string Class
+    {
+        get => _class;
+        set => Set(ref _class, value);
+    }
+
     public string AxialLigand
     {
         get => _axialLigand;
         set => Set(ref _axialLigand, value);
     }
-    private string _ligand = "";
+
     public string Ligand
     {
         get => _ligand;
         set => Set(ref _ligand, value);
     }
-    
-    private string _coSolv = "";
+
     public string CoSolv
     {
         get => _coSolv;
         set => Set(ref _coSolv, value);
     }
 
-    private int _coordNo = 0;
-    public  int CoordNo
+    public int CoordNo
     {
         get => _coordNo;
         set => Set(ref _coordNo, value);
     }
-    
-    public string Group { get;set; }
-    
-    private int _substNo = 0;
-    public  int SubstNo
+
+    public string Group { get; set; }
+
+    public int SubstNo
     {
         get => _substNo;
         set => Set(ref _substNo, value);
     }
 
-    
+
     [JsonIgnore]
     public string WorkingDir
     {
@@ -80,7 +92,7 @@ public class MainViewModel : BaseViewModel
         set => Set(ref _workingDir, value, LoadFiles);
     }
 
-    
+
     [JsonIgnore]
     public Stack<string> Files
     {
@@ -91,14 +103,12 @@ public class MainViewModel : BaseViewModel
     /// <summary>
     ///     3D Representation of Atoms
     /// </summary>
-    /// 
     [JsonIgnore]
     public ObservableCollection<Atom3D> Atoms3D { get; } = new();
 
     /// <summary>
     ///     3D Representation of Bonds
     /// </summary>
-    /// 
     [JsonIgnore]
     public ObservableCollection<Bond3D> Bonds3D { get; } = new();
 
@@ -110,6 +120,7 @@ public class MainViewModel : BaseViewModel
         foreach (var atom in CurrentMolecule.Atoms) Atoms3D.Add(new Atom3D(atom));
         foreach (var bond in CurrentMolecule.Bonds) Bonds3D.Add(new Bond3D(bond));
     }
+
     private void LoadFiles()
     {
         var supported = new[] {"cif", "xyz", "mol2"};
@@ -124,6 +135,7 @@ public class MainViewModel : BaseViewModel
     public void Next()
     {
         Load(Files.Peek());
+        Title = Path.GetFileName(_currentMolecule.Title);
         OnPropertyChanged(nameof(Files));
     }
 }
--- FileClassification/MainWindow.xaml.cs
+++ FileClassification/MainWindow.xaml.cs
@@ -9,87 +9,86 @@ using ChemSharp.Molecules.HelixToolkit;
 using FileClassification.ViewModel;
 using HelixToolkit.Wpf;
 using Microsoft.Win32;
-using Path = System.IO.Path;
 
+namespace FileClassification;
 
-namespace FileClassification
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
 {
+    public MainWindow()
+    {
+        InitializeComponent();
+        ViewModel = new MainViewModel();
+        DataContext = ViewModel;
+        ViewModel.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(ViewModel.CurrentMolecule))
+                Viewport3D.CameraController.CameraTarget =
+                    MathV.Centroid(ViewModel.CurrentMolecule.Atoms.Select(a => a.Location).ToList()).ToPoint3D();
+        };
+    }
+
+    public MainViewModel ViewModel { get; }
+
+    private void Search_OnClick(object sender, RoutedEventArgs e)
+    {
+        var ofd = new OpenFileDialog
+        {
+            FileName = "Select Folder.",
+            InitialDirectory = PathTextBox.Text,
+            ValidateNames = false,
+            CheckFileExists = false,
+            CheckPathExists = false
+        };
+        if (ofd.ShowDialog(this) != true) return;
+        PathTextBox!.Text = Path.GetDirectoryName(ofd.FileName) ?? "";
+        PathTextBox.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+    }
+
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Handles Atom selection
     /// </summary>
-    public partial class MainWindow
-    { 
-        public MainViewModel ViewModel { get; }
-        public MainWindow()
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Viewport3D_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var hits = Viewport3D.Viewport.FindHits(e.GetPosition(Viewport3D));
+        var shift = (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift;
+        var alt = (Keyboard.Modifiers & ModifierKeys.Alt) == ModifierKeys.Alt;
+        foreach (var hit in hits.OrderBy(s => s.Distance))
         {
-            InitializeComponent();
-            ViewModel = new MainViewModel();
-            DataContext = ViewModel;
-            ViewModel.PropertyChanged += (sender, args) =>
+            if (hit.Visual.GetType() != typeof(Atom3D)) continue;
+            if (hit.Visual is not Atom3D av3d) continue;
+
+            //write data
+            if (shift && alt) ViewModel.CoordNo++;
+            else if (shift) ViewModel.AxialLigand = av3d.Atom.Symbol;
+            else if (alt) ViewModel.SubstNo++;
+            else
             {
-                if (args.PropertyName == nameof(ViewModel.CurrentMolecule))
-                {
-                    Viewport3D.CameraController.CameraTarget =
-                        MathV.Centroid(ViewModel.CurrentMolecule.Atoms.Select(a => a.Location).ToList()).ToPoint3D();
-                }
-            };
-        }
-        private void Search_OnClick(object sender, RoutedEventArgs e)
-        {
-            var ofd = new OpenFileDialog()
-            {
-                FileName = "Select Folder.",
-                InitialDirectory = PathTextBox.Text,
-                ValidateNames = false,
-                CheckFileExists = false,
-                CheckPathExists = false
-            };
-            if (ofd.ShowDialog(this) != true) return;
-            PathTextBox!.Text = Path.GetDirectoryName(ofd.FileName) ?? "";
-            PathTextBox.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
-        }
-        
-        /// <summary>
-        /// Handles Atom selection
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Viewport3D_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var hits = Viewport3D.Viewport.FindHits(e.GetPosition(Viewport3D));
-            var shift = (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift;
-            var alt = (Keyboard.Modifiers & ModifierKeys.Alt) == ModifierKeys.Alt;
-            foreach (var hit in hits.OrderBy(s => s.Distance))
-            {
-                if (hit.Visual.GetType() != typeof(Atom3D)) continue;
-                if (hit.Visual is not Atom3D av3d) continue;
-                
-                //write data
-                if (shift && alt) ViewModel.CoordNo++;
-                else if (shift) ViewModel.AxialLigand = av3d.Atom.Symbol;
-                else if (alt) ViewModel.SubstNo++;
-                else
-                {
-                    ViewModel.Group = av3d.Atom.Block != "f-Block" ? av3d.Atom.Group.ToString() : "Ln";
-                    ViewModel.Metal = av3d.Atom.Symbol;
-                }
+                ViewModel.Group = av3d.Atom.Block != "f-Block" ? av3d.Atom.Group.ToString() : "Ln";
+                ViewModel.Metal = av3d.Atom.Symbol;
             }
         }
+    }
 
-        private void SubmitClick(object sender, RoutedEventArgs e)
-        {
-            var m = ViewModel.Metal;
-            var clazz = ClassComboBox.Text;
-            if(!ViewModel.Classes.Contains(clazz)) ViewModel.Classes.Add(clazz);
-            var path = $"{ViewModel.WorkingDir}\\{clazz}\\{m}\\";
-            if (!File.Exists(path)) Directory.CreateDirectory(path);
-            
-            var filename = ViewModel.Files.Pop();
-            var rawFilename =Path.GetFileName(filename);
-            
-            File.Move(filename,  path + rawFilename);
-            File.WriteAllText(path+rawFilename + ".meta.json",JsonSerializer.Serialize(ViewModel, new JsonSerializerOptions { WriteIndented = true }));
-            ViewModel.Next();
-        }
+    private void SubmitClick(object sender, RoutedEventArgs e)
+    {
+        var m = ViewModel.Metal;
+        var clazz = ClassComboBox.Text;
+        ViewModel.Class = clazz;
+        if (!ViewModel.Classes.Contains(clazz)) ViewModel.Classes.Add(clazz);
+        var path = $"{ViewModel.WorkingDir}\\{clazz}\\{m}\\";
+        if (!File.Exists(path)) Directory.CreateDirectory(path);
+
+        var filename = ViewModel.Files.Pop();
+        var rawFilename = Path.GetFileName(filename);
+
+        File.Move(filename, path + rawFilename);
+        File.WriteAllText(path + rawFilename + ".meta.json",
+                          JsonSerializer.Serialize(ViewModel, new JsonSerializerOptions {WriteIndented = true}));
+        ViewModel.Next();
     }
 }
