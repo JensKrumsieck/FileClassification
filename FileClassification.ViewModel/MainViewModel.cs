ac413f495aad3fa7afc07323d2d8dd7f8fdce1a7
Jens Krumsieck <jenso666@live.de>
2022-08-18 10:36:26 +0200

    initial

---
+++ FileClassification.ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using ChemSharp.Molecules;
+using ChemSharp.Molecules.HelixToolkit;
+using TinyMVVM;
+
+namespace FileClassification.ViewModel;
+
+public class MainViewModel : BaseViewModel
+{
+    public ObservableCollection<string> Classes { get; } = new();
+
+    private Stack<string> _files;
+    private string _workingDir;
+
+    private Molecule _currentMolecule;
+
+    public Molecule CurrentMolecule
+    {
+        get => _currentMolecule;
+        set => Set(ref _currentMolecule, value);
+    }
+
+    private string _selectedAtom = "";
+    public string SelectedAtom
+    {
+        get => _selectedAtom;
+        set => Set(ref _selectedAtom, value);
+    }
+    
+    
+    public string WorkingDir
+    {
+        get => _workingDir;
+        set => Set(ref _workingDir, value, LoadFiles);
+    }
+
+    public Stack<string> Files
+    {
+        get => _files;
+        set => Set(ref _files, value);
+    }
+
+    /// <summary>
+    ///     3D Representation of Atoms
+    /// </summary>
+    public ObservableCollection<Atom3D> Atoms3D { get; } = new();
+
+    /// <summary>
+    ///     3D Representation of Bonds
+    /// </summary>
+    public ObservableCollection<Bond3D> Bonds3D { get; } = new();
+
+    private void Load(string filename)
+    {
+        Atoms3D.Clear();
+        Bonds3D.Clear();
+        CurrentMolecule = MoleculeFactory.Create(filename);
+        foreach (var atom in CurrentMolecule.Atoms) Atoms3D.Add(new Atom3D(atom));
+        foreach (var bond in CurrentMolecule.Bonds) Bonds3D.Add(new Bond3D(bond));
+    }
+    private void LoadFiles()
+    {
+        var supported = new[] {"cif", "xyz", "mol2"};
+        if (!Directory.Exists(WorkingDir) || string.IsNullOrEmpty(WorkingDir)) return;
+        Files = new Stack<string>(supported.Select(ext => "*." + ext)
+                                           .SelectMany(f => Directory.EnumerateFiles(WorkingDir, f,
+                                                           SearchOption.TopDirectoryOnly)).Reverse()
+                                 );
+        Next();
+    }
+
+    public void Next()
+    {
+        Load(Files.Peek());
+        OnPropertyChanged(nameof(Files));
+    }
+}
---
+++ FileClassification/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ChemSharp.Mathematics;
+using ChemSharp.Molecules.HelixToolkit;
+using FileClassification.ViewModel;
+using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+
+namespace FileClassification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    { 
+        public MainViewModel ViewModel { get; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
+            ViewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(ViewModel.CurrentMolecule))
+                {
+                    Viewport3D.CameraController.CameraTarget =
+                        MathV.Centroid(ViewModel.CurrentMolecule.Atoms.Select(a => a.Location).ToList()).ToPoint3D();
+                }
+            };
+        }
+        private void Search_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog()
+            {
+                FileName = "Select Folder.",
+                InitialDirectory = PathTextBox.Text,
+                ValidateNames = false,
+                CheckFileExists = false,
+                CheckPathExists = false
+            };
+            if (ofd.ShowDialog(this) != true) return;
+            PathTextBox!.Text = Path.GetDirectoryName(ofd.FileName) ?? "";
+            PathTextBox.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+        }
+        
+        /// <summary>
+        /// Handles Atom selection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Viewport3D_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var hits = Viewport3D.Viewport.FindHits(e.GetPosition(Viewport3D));
+            foreach (var hit in hits.OrderBy(s => s.Distance))
+            {
+                if (hit.Visual.GetType() != typeof(Atom3D)) continue;
+                var av3d = hit.Visual as Atom3D;
+                if (av3d != null) ViewModel.SelectedAtom = av3d.Atom.Symbol;
+            }
+        }
+
+        private void SubmitClick(object sender, RoutedEventArgs e)
+        {
+            var m = MetalTextBox.Text;
+            var clazz = ClassComboBox.Text;
+            if(!ViewModel.Classes.Contains(clazz)) ViewModel.Classes.Add(clazz);
+            var path = $"{ViewModel.WorkingDir}\\{clazz}\\{m}\\";
+            if (!File.Exists(path)) Directory.CreateDirectory(path);
+            
+            var filename = ViewModel.Files.Pop();
+            var rawFilename =Path.GetFileName(filename);
+            
+            File.Move(filename,  path + rawFilename);
+            ViewModel.Next();
+        }
+    }
+}
